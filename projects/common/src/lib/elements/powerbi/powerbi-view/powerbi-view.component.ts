4c0ddba17aa91d2cf87eef840d68b0116c1bd720
ttrichar <trevor.richardson@fathym.com>
2020-02-26 16:05:00 -0700

    Create powerbi-view sln

---
+++ projects/common/src/lib/elements/powerbi/powerbi-view/powerbi-view.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Injector } from '@angular/core';
+import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+
+export class LcuReportingPowerbiViewElementState {}
+
+export class LcuReportingPowerbiViewContext extends LCUElementContext<LcuReportingPowerbiViewElementState> {}
+
+export const SELECTOR_LCU_REPORTING_POWERBI_VIEW_ELEMENT = 'lcu-reporting-powerbi-view-element';
+
+@Component({
+  selector: SELECTOR_LCU_REPORTING_POWERBI_VIEW_ELEMENT,
+  templateUrl: './powerbi-view.component.html',
+  styleUrls: ['./powerbi-view.component.scss']
+})
+export class LcuReportingPowerbiViewElementComponent extends LcuElementComponent<LcuReportingPowerbiViewContext> implements OnInit {
+  //  Fields
+
+  //  Properties
+
+  //  Constructors
+  constructor(protected injector: Injector) {
+    super(injector);
+  }
+
+  //  Life Cycle
+  public ngOnInit() {
+    super.ngOnInit();
+  }
+
+  //  API Methods
+
+  //  Helpers
+}
